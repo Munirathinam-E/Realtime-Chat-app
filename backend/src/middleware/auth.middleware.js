6bf72c4282192f47df6c12a6cbcaa28e152c4ebc
Munirathinam-E <munirathinam6724.com>
2025-02-23 11:47:24 +0530

    Your commit message

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -5,7 +5,7 @@ export const protectRoute=async(req,res,next)=>{
     try{
         const token=req.cookies.jwt;
 
-        if(!token){
+        if(!token){ 
             return res.status(401).json({message:"Unauthorized -No Token Provides"});
         }
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
